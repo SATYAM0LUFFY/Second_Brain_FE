47462bd92cb196fb955ddb6768baab17ceca149a
Satyam Singh <satyamsinghu123123@gmail.com>
2025-01-02 12:43:12 +0530

    correcting_fe_url

--- src/config.tsx
+++ src/config.tsx
@@ -1,3 +1,3 @@
 export const backendURL : string = "https://second-brain-be-s.onrender.com";
-export const frontendURL : string = "http://localhost:5173";
+export const frontendURL : string = "https://second-brain-fesatyam.vercel.app";
 export const token  = localStorage.getItem("token");
